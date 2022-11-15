54c044ddc656aa7e80140ca90bd5f3e3eed05d3a
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-11-15 08:57:42 -0700

    Added verification for a secure password

--- src/controllers/userAccount.js
+++ src/controllers/userAccount.js
@@ -6,32 +6,26 @@ const { Router } = require("express");
 //Register a new user
 const registration = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password, verifyPassword } = req.body;
     //Verify all required fields are filled out
-    if (!firstName || !lastName || !email || !password || !verifyPassword) {
+    if (!firstName || !lastName || !email || !password || !verifyPassword) 
       return res.status(400).send("Please fill out all required fields");
-    }
     //Validation for password
-    if (password.length < 8) {
+    if (password.length < 8) 
       return res
         .status(400)
         .send("Password must be at least 8 characters long");
-    }
-    if (password.search(/[a-z]/gi) < 1) {
+    if (password.search(/[a-z]/gi) < 1) 
       return res
         .status(400)
         .send("Password must contain one lower case letter");
-    }
-    if (password.search(/[A-Z]/gi) < 1) {
+    if (password.search(/[A-Z]/gi) < 1) 
       return res.status(400).send("Password must contain one uppercase letter");
-    }
-    if (password.search(/[0-9]/gi) < 1) {
+    if (password.search(/[0-9]/gi) < 1) 
       return res.status(400).send("Password must contain at least one digit");
-    }
     //Verify that both passwords match
-    if (password !== verifyPassword) {
+    if (password !== verifyPassword) 
       return res.status(400).send("Password does not match");
-    }
     //Check to see if user already exists
     const existingUser = await findByEmail({ email: email });
     if (existingUser) {
@@ -60,3 +54,5 @@ const registration = async (req, res) => {
     res.status(500).send();
   }
 };
+
+module.exports = { registration }
