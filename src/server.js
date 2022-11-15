927a4a978b6f7562905a1346f975679b09429422
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-11-15 08:58:07 -0700

    added registration route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require('./config/mongoose');
+const userRoutes = require('./routes/user');
 
 PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/user", userRoutes)
 
 app.get("/", async (req, res) => {
     res.send("This will soon be the server for the Ski Crud application")
