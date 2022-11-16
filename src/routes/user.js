2ec9c3ecbcca3bab978615ff2bdc1d18de38ed46
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-11-16 08:59:04 -0700

    endpoints for user login and logout

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registration } = require('../controllers/userAccount');
+const { registration, login, logout } = require('../controllers/userAccount');
 
 //Register a new user
 router.post("/register", registration);
+//log in a new user
+router.post("/login", login)
+//log the user out
+router.get("/logout", logout);
 
 module.exports = router;
